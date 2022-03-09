1dba4493549d7ba82578aa491934fd436e180e4e
Yuliya Yuzeeva <akuliniya@mail.ru>
2022-03-09 23:15:53 +0300

    HW03-L06-annotations 1. Из TestRunner убраны статические поля 2. Методы заранее распределяются по спискам. 3. AfterMethod запускается даже в том случае, если тесты или BeforeMethod не прошел.

--- HW03-L06-annotations/src/main/java/homework/TestForDemo.java
+++ HW03-L06-annotations/src/main/java/homework/TestForDemo.java
@@ -7,6 +7,7 @@ public class TestForDemo {
         System.out.println("Before test");
     }
 
+
     @After
     public void afterAllMethod() {
         System.out.println("After test");
@@ -21,7 +22,6 @@ public class TestForDemo {
     @Test
     public void testMethod2() {
         System.out.println("Test2 running");
-        throw new RuntimeException();
 
     }
 
@@ -35,6 +35,12 @@ public class TestForDemo {
     @Test
     public void testMethod4() {
         System.out.println("Test4 running");
+
+    }
+
+    @Test
+    public void testMethod5() {
+        System.out.println("Test5 running");
 
     }
 }
--- HW03-L06-annotations/src/main/java/homework/TestRunner.java
+++ HW03-L06-annotations/src/main/java/homework/TestRunner.java
@@ -4,109 +4,139 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRunner {
-    private final static Class<Before> BEFORE_ANNOTATION = Before.class;
-    private final static Class<After> AFTER_ANNOTATION = After.class;
-    private final static Class<Test> TEST_ANNOTATION = Test.class;
-
-    private static int tests = 0;
-    private static int passed = 0;
-    private static int failed = 0;
 
     public static void performTesting(String testClassName) {
         try {
-            runTests(testClassName);
-            printStatistics(testClassName);
-        }catch (Throwable e){
+            printStatistics(testClassName, runTests(testClassName));
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            System.out.println("Во время запуска тестов возникла ошибка.");
+            StackTraceElement[] list = e.getStackTrace();
+            for (StackTraceElement s : list) {
+                System.out.println(s);
+            }
+        } catch (Throwable e) {
             System.out.println("Во время запуска тестов возникла ошибка.");
             System.out.println(e.getMessage() + " не найден.");
             StackTraceElement[] list = e.getStackTrace();
-            for (StackTraceElement s : list){
+            for (StackTraceElement s : list) {
                 System.out.println(s);
             }
         }
     }
 
-    private static void runTests(String testClassName) throws ClassNotFoundException, NoSuchMethodException, InstantiationException {
+    private static List<Integer> runTests(String testClassName) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException, IllegalAccessException {
+        List<Integer> testRunResults = new ArrayList<>();
+        int testsCounter = 0;
+        int passedCounter = 0;
+        int failedCounter = 0;
+
         Class<?> clazz = Class.forName(testClassName);
         Method[] methods = clazz.getMethods();
         Constructor<?> constructor = clazz.getConstructor();
 
-        for (Method method : methods) {
-            if (isTestMethod(method)) {
-                tests++;
-                try {
-                    Object test = constructor.newInstance();
-                    getBeforeMethod(methods).invoke(test);
-                    method.invoke(test);
-                    passed++;
-                    getAfterMethod(methods).invoke(test);
-                } catch (InvocationTargetException wrappedExc) {
-                    failed++;
-                    Throwable exc = wrappedExc.getCause();
-                    System.out.println(method + " failed: " + exc);
-                } catch (IllegalAccessException exc) {
-                    failed++;
-                    System.out.println("Invalid @Test: " + method);
+        List<Method> beforeMethods = getBeforeMethods(methods);
+        List<Method> testMethods = getTestMethods(methods);
+        List<Method> afterMethods = getAfterMethods(methods);
+
+        for (Method testMethod : testMethods) {
+            Object test = constructor.newInstance();
+            testsCounter++;
+
+            for (Method beforeMethod : beforeMethods) {
+                beforeMethod.invoke(test);
+            }
+
+            try {
+                testMethod.invoke(test);
+                passedCounter++;
+            } catch (InvocationTargetException wrappedExc) {
+                failedCounter++;
+                Throwable exc = wrappedExc.getCause();
+                System.out.println(testMethod + " failed: " + exc);
+            } catch (IllegalAccessException exc) {
+                failedCounter++;
+                System.out.println("Invalid @Test: " + testMethod);
+            } finally {
+                for (Method afterMethod : afterMethods) {
+                    afterMethod.invoke(test);
                 }
             }
         }
+
+        testRunResults.add(testsCounter);
+        testRunResults.add(passedCounter);
+        testRunResults.add(failedCounter);
+
+        return testRunResults;
     }
 
-    private static void printStatistics(String testClassName) {
-        System.out.println(testClassName + ": Выполнено тестов: " + tests);
-        System.out.println(testClassName + ": Пройдено: " + passed);
-        System.out.println(testClassName + ": Упало: " + failed);
+    private static void printStatistics(String testClassName, List<Integer> testRunResults) {
+        System.out.println(testClassName + ": Выполнено тестов: " + testRunResults.get(0));
+        System.out.println(testClassName + ": Пройдено: " + testRunResults.get(1));
+        System.out.println(testClassName + ": Упало: " + testRunResults.get(2));
     }
 
-    private static Method getBeforeMethod(Method[] methods){
-        Method beforeMethod = null;
+    private static List<Method> getBeforeMethods(Method[] methods) {
+        List<Method> beforeMethods = new ArrayList<>();
         for (Method method : methods) {
-            if (isBeforeMethod(method)){
-                beforeMethod = method;
+            if (isBeforeMethod(method)) {
+                beforeMethods.add(method);
             }
         }
-        return beforeMethod;
+        return beforeMethods;
     }
 
-    private static Method getAfterMethod(Method[] methods){
-        Method afterMethod = null;
+    private static List<Method> getAfterMethods(Method[] methods) {
+        List<Method> afterMethods = new ArrayList<>();
         for (Method method : methods) {
-            if (isAfterMethod(method)){
-                afterMethod = method;
+            if (isAfterMethod(method)) {
+                afterMethods.add(method);
             }
         }
-        return afterMethod;
+        return afterMethods;
+    }
+
+    private static List<Method> getTestMethods(Method[] methods) {
+        List<Method> testMethods = new ArrayList<>();
+        for (Method method : methods) {
+            if (isTestMethod(method)) {
+                testMethods.add(method);
+            }
+        }
+        return testMethods;
     }
 
     private static boolean isBeforeMethod(Method method) {
         boolean isBeforeMethod = false;
         Annotation[] annotations = method.getDeclaredAnnotations();
         for (Annotation annotation : annotations) {
-            if (annotation.annotationType().equals(BEFORE_ANNOTATION))
+            if (annotation.annotationType().equals(Before.class))
                 isBeforeMethod = true;
         }
-        return  isBeforeMethod;
+        return isBeforeMethod;
     }
 
     private static boolean isAfterMethod(Method method) {
         boolean isAfterMethod = false;
         Annotation[] annotations = method.getDeclaredAnnotations();
         for (Annotation annotation : annotations) {
-            if (annotation.annotationType().equals(AFTER_ANNOTATION))
+            if (annotation.annotationType().equals(After.class))
                 isAfterMethod = true;
         }
-        return  isAfterMethod;
+        return isAfterMethod;
     }
 
     private static boolean isTestMethod(Method method) {
         boolean isAfterMethod = false;
         Annotation[] annotations = method.getDeclaredAnnotations();
         for (Annotation annotation : annotations) {
-            if (annotation.annotationType().equals(TEST_ANNOTATION))
+            if (annotation.annotationType().equals(Test.class))
                 isAfterMethod = true;
         }
-        return  isAfterMethod;
+        return isAfterMethod;
     }
 }
